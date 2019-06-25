a2df202806c9456921f894da7feb611566606b33
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-25 20:19:59 +0800

    Update 笔记.sql

--- 笔记.sql
+++ 笔记.sql
@@ -62,3 +62,44 @@ gender varchar(2)
 insert into hhh(id,name,gender) values (1,'jack','男')
 
 
+
+9.显示人名字和部门名称
+
+
+emp dept
+select emp.ename,dept.dname from emp,dept where emp.deptno = dept.deptno;   --内连接
+select emp.ename,dept.dname from inner join dept on emp.deptno = dept.deptno;  -- 内连接的标准写法
+
+左连接和右连接：（哪边完全显示就是什么连接）
+编号 名称 班级          编号   名称
+1   张三    java1        java1    java北郊
+
+查询所有学生的成绩，如果没有也要显示出来
+将学生表放在左边，没有也能显示出来
+左右可以互换：左换右，表换表；右换左，右表换左表（特点）
+
+
+innodb不支持全索引
+memory：支持btree，支持hashindex
+
+
+10.
+索引：不加内存，不改程序，不调sql，可以提高查询速度成百上千倍，查询速度的提高前提是牺牲了插入，
+更新，删除的速度为代价的，这些写操作，增加了大量的io，所以它的价值，在于提高海量数据的检索速度
+
+常见索引分为：主键索引（primary key），唯一索引（unique），普通索引（index），全文索引（fulltext）--解决中子索引的问题
+
+案例：先整一个海量表，在查询的时候，看看索引问题
+构建模拟一个8000000条记录的数据
+
+delimiter ;
+
+&&意味着开始和结束   ，procedure过程，call为调用函数的意思 select * from 表 add index（depton);
+
+主键索引:
+primary key，主键本身就是一个索引，主键可以指定多列
+唯一索引：unique = unique key
+
+普通索引：前两种添加方式，会自动把列名作为索引名字，而第三种会手动添加，因为索引是隶属于某个表的，所以必须首先告诉是在哪一个表的，否则就会像前两种一样自动将列认为索引名字，
+而非手动指定    特点：可以有多个，即区别于主键索引额和唯一索引的唯一性（不能有多个）
+
